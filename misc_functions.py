ce97ad8fef9d2f8561577df6078d4febc9b14546
tarungupta83 <tarungupta83@gmail.com>
2021-01-06 15:36:47 +0530

    Mistakenly called get_all_files to check is this working corrected
    "Well passed"

--- misc_functions.py
+++ misc_functions.py
@@ -76,5 +76,3 @@ def divide_data(files_folder, no_of_files, make_folder):
             file_path = os.path.join(dirname, filename)
             shutil.move(file_path, dirname_path)
             print("Moved File " , file_path , " to ", dirname_path)
-
-get_file("https://images-na.ssl-images-amazon.com/images/I/71eXtqkndlL.jpg")
